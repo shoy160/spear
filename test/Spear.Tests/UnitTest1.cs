8e472454df0198ba06f87fce2dfbfdee3798463c
shoy <634330628@qq.com>
2019-02-19 11:06:22 +0800

    update version 0.1.2

--- test/Spear.Tests/UnitTest1.cs
+++ test/Spear.Tests/UnitTest1.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Spear.Tests
@@ -8,6 +14,16 @@ namespace Spear.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            //响应式编程
+            var list = Enumerable.Range(1, 100).ToObservable(NewThreadScheduler.Default);
+            var subject = new Subject<int>();
+            subject.Subscribe((temperature) => Console.WriteLine($"当前温度：{temperature}"));//订阅subject
+            subject.Subscribe((temperature) => Console.WriteLine($"嘟嘟嘟，当前水温：{temperature}"));//订阅subject
+            list.Subscribe(subject);
+            //list.Wait();
+
+            var observable = Observable.Return("shay");
+            observable.Subscribe(Console.WriteLine);
         }
     }
 }
